f3a5c53017e28f69e81c80951acf210c1c2d84fc
d-mozulyov <softforyou@inbox.ru>
2016-05-29 16:40:25 +0300

    MemoryUsageTest: done

--- demos/MemoryUsageTest/MemoryUsageTest.pas
+++ demos/MemoryUsageTest/MemoryUsageTest.pas
@@ -3,7 +3,7 @@ unit MemoryUsageTest;
 interface
 
 uses
-  Types, Windows, SysUtils;
+  Types, Windows, PSApi, SysUtils;
   
   
 procedure RUN;  
@@ -11,9 +11,39 @@ procedure RUN;
 implementation
 
 
-procedure RUN;
+function GetMemoryUsage: NativeUInt;
+var
+  MemCounters: TProcessMemoryCounters;
 begin
-  Writeln(ChangeFileExt(ExtractFileName(ParamStr(0)), ''));
+  MemCounters.cb := SizeOf(MemCounters);
+  if (not GetProcessMemoryInfo(GetCurrentProcess, @MemCounters, SizeOf(MemCounters))) then
+    RaiseLastOSError;
+
+  Result := MemCounters.WorkingSetSize
+end;
+
+procedure RUN;
+const
+  MB = 1024 * 1024;
+var
+  Name: string;
+  Size, i: NativeUInt;
+  MemoryUsage: NativeUInt;
+begin
+  Name := ChangeFileExt(ExtractFileName(ParamStr(0)), '');
+  Size := StrToInt(Copy(Name, LastDelimiter('_', Name) + 1, MaxInt));
+
+  MemoryUsage := GetMemoryUsage;
+  for i := 1 to (100 * MB) div Size do
+  begin
+    GetMemory(Size);
+  end;
+  MemoryUsage := GetMemoryUsage - MemoryUsage;
+
+  Writeln(Format('%s: %0.2fMb', [Name, MemoryUsage / MB]));
+  {$ifdef DEBUG}
+    Readln;
+  {$endif}
 end;
 
  
